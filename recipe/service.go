cad45fe8475bb60a9da905b98d7cd28ee5915c99
Robert Schumann <dukfaar@gmail.com>
2018-11-03 10:40:22 +0100

    use new goUtils

--- recipe/service.go
+++ recipe/service.go
@@ -5,6 +5,7 @@ import (
 	"github.com/globalsign/mgo/bson"
 
 	"github.com/dukfaar/goUtils/eventbus"
+	"github.com/dukfaar/goUtils/service"
 )
 
 type Service interface {
@@ -32,23 +33,25 @@ type Service interface {
 }
 
 type MgoService struct {
-	db         *mgo.Database
-	collection *mgo.Collection
-	eventbus   eventbus.EventBus
+	service.BaseMgoServiceWithQuery
+	db       *mgo.Database
+	eventbus eventbus.EventBus
 }
 
 func NewMgoService(db *mgo.Database, eventbus eventbus.EventBus) *MgoService {
 	return &MgoService{
-		db:         db,
-		collection: db.C("recipes"),
-		eventbus:   eventbus,
+		BaseMgoServiceWithQuery: service.BaseMgoServiceWithQuery{
+			Collection: db.C("recipes"),
+		},
+		db:       db,
+		eventbus: eventbus,
 	}
 }
 
 func (s *MgoService) Create(model *Model) (*Model, error) {
 	model.ID = bson.NewObjectId()
 
-	err := s.collection.Insert(model)
+	err := s.Collection.Insert(model)
 
 	if err == nil {
 		s.eventbus.Emit("recipe.created", model)
@@ -57,27 +60,9 @@ func (s *MgoService) Create(model *Model) (*Model, error) {
 	return model, err
 }
 
-func (s *MgoService) MakeBaseQuery() bson.M {
-	return bson.M{}
-}
-
-func (s *MgoService) MakeListQuery(query bson.M, before *string, after *string) {
-	if after != nil {
-		query["_id"] = bson.M{
-			"$gt": bson.ObjectIdHex(*after),
-		}
-	}
-
-	if before != nil {
-		query["_id"] = bson.M{
-			"$lt": bson.ObjectIdHex(*before),
-		}
-	}
-}
-
 func (s *MgoService) PerformQuery(query bson.M) *Model {
 	var result Model
-	s.collection.Find(query).One(&result)
+	s.Collection.Find(query).One(&result)
 	return &result
 }
 
@@ -92,54 +77,19 @@ func (s *MgoService) PerformListQuery(query bson.M, first *int32, last *int32, b
 	}
 
 	if last != nil {
-		count, _ := s.collection.Find(query).Count()
+		count, _ := s.Collection.Find(query).Count()
 
 		limit = int(*last)
 		skip = count - limit
 	}
 
 	var result []Model
-	err := s.collection.Find(query).Skip(skip).Limit(limit).All(&result)
+	err := s.Collection.Find(query).Skip(skip).Limit(limit).All(&result)
 	return result, err
 }
 
-func (s *MgoService) HasElementBeforeIDWithQuery(inquery bson.M, id string) (bool, error) {
-	query := bson.M{}
-
-	for k, v := range inquery {
-		query[k] = v
-	}
-
-	query["_id"] = bson.M{
-		"$lt": bson.ObjectIdHex(id),
-	}
-
-	count, err := s.collection.Find(query).Count()
-	return count > 0, err
-}
-
-func (s *MgoService) HasElementAfterIDWithQuery(inquery bson.M, id string) (bool, error) {
-	query := bson.M{}
-
-	for k, v := range inquery {
-		query[k] = v
-	}
-
-	query["_id"] = bson.M{
-		"$gt": bson.ObjectIdHex(id),
-	}
-
-	count, err := s.collection.Find(query).Count()
-	return count > 0, err
-}
-
-func (s *MgoService) CountWithQuery(query bson.M) (int, error) {
-	count, err := s.collection.Find(query).Count()
-	return count, err
-}
-
 func (s *MgoService) Update(id string, input interface{}) (*Model, error) {
-	err := s.collection.UpdateId(bson.ObjectIdHex(id), input)
+	err := s.Collection.UpdateId(bson.ObjectIdHex(id), input)
 
 	if err != nil {
 		return nil, err
@@ -157,7 +107,7 @@ func (s *MgoService) Update(id string, input interface{}) (*Model, error) {
 }
 
 func (s *MgoService) DeleteByID(id string) (string, error) {
-	err := s.collection.RemoveId(bson.ObjectIdHex(id))
+	err := s.Collection.RemoveId(bson.ObjectIdHex(id))
 
 	if err == nil {
 		s.eventbus.Emit("recipe.deleted", id)
@@ -169,36 +119,9 @@ func (s *MgoService) DeleteByID(id string) (string, error) {
 func (s *MgoService) FindByID(id string) (*Model, error) {
 	var result Model
 
-	err := s.collection.FindId(bson.ObjectIdHex(id)).One(&result)
+	err := s.Collection.FindId(bson.ObjectIdHex(id)).One(&result)
 
 	return &result, err
-}
-
-func (s *MgoService) HasElementBeforeID(id string) (bool, error) {
-	query := bson.M{}
-
-	query["_id"] = bson.M{
-		"$lt": bson.ObjectIdHex(id),
-	}
-
-	count, err := s.collection.Find(query).Count()
-	return count > 0, err
-}
-
-func (s *MgoService) HasElementAfterID(id string) (bool, error) {
-	query := bson.M{}
-
-	query["_id"] = bson.M{
-		"$gt": bson.ObjectIdHex(id),
-	}
-
-	count, err := s.collection.Find(query).Count()
-	return count > 0, err
-}
-
-func (s *MgoService) Count() (int, error) {
-	count, err := s.collection.Find(bson.M{}).Count()
-	return count, err
 }
 
 func (s *MgoService) List(first *int32, last *int32, before *string, after *string) ([]Model, error) {
@@ -226,13 +149,13 @@ func (s *MgoService) List(first *int32, last *int32, before *string, after *stri
 	}
 
 	if last != nil {
-		count, _ := s.collection.Find(query).Count()
+		count, _ := s.Collection.Find(query).Count()
 
 		limit = int(*last)
 		skip = count - limit
 	}
 
 	var result []Model
-	err := s.collection.Find(query).Skip(skip).Limit(limit).All(&result)
+	err := s.Collection.Find(query).Skip(skip).Limit(limit).All(&result)
 	return result, err
 }
